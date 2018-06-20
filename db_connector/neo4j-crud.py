8d6962ffb9a464c11d31928a3260d3b8cafe3ac4
concept-inversion <santosh.pandey2222@gmail.com>
2018-06-20 20:15:18 +0545

    adds database connection

--- db_connector/neo4j-crud.py
+++ db_connector/neo4j-crud.py
@@ -1,6 +1,13 @@
 import json, datetime
 import sys
 from neo4j.v1 import basic_auth, GraphDatabase
-sys.path.append("../")
-config = json.loads(open("../config/config.json").read())
-username , password = config["username"], password["password"]
+
+class neo4j_crud:
+    def __init__(self, *args, **kwargs):
+        config = json.loads(open("./config/config.json").read())
+        username, password = config["username"], config["password"]
+        url = "bolt://localhost:7687"
+        self.driver= GraphDatabase.driver(url,auth=basic_auth(username,password))
+
+if __name__=="__main__":
+    db= neo4j_crud()
