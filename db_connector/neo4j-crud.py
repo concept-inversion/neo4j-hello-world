73cbea41a2b0c47f7181c55a856b4951b426bcfa
concept-inversion <santosh.pandey2222@gmail.com>
2018-07-24 12:33:36 +0545

    changes loads to load for file llike read json

--- db_connector/neo4j-crud.py
+++ db_connector/neo4j-crud.py
@@ -1,10 +1,11 @@
 import json, datetime
-import sys
+import sys,os
 from neo4j.v1 import basic_auth, GraphDatabase
-
+os.path.join("../config/config.json")
 class neo4j_crud:
     def __init__(self, *args, **kwargs):
-        config = json.loads(open("./config/config.json").read())
+        #config = json.loads(open("./config/config.json").read())
+        config = json.load(open("../config/config.json"))
         username, password = config["username"], config["password"]
         url = "bolt://localhost:7687"
         self.driver= GraphDatabase.driver(url,auth=basic_auth(username,password))
