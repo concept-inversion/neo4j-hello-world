03d012b2961f85785e533fbef7e10591db5ee5e1
concept-inversion <santosh.pandey2222@gmail.com>
2018-07-24 14:52:10 +0545

    adds close function and read function

--- db_connector/neo4j-crud-native.py
+++ db_connector/neo4j-crud-native.py
@@ -2,6 +2,7 @@ import json, datetime
 import sys,os
 from neo4j.v1 import basic_auth, GraphDatabase
 os.path.join("../config/config.json")
+
 class neo4j_crud:
     def __init__(self, *args, **kwargs):
         #config = json.loads(open("./config/config.json").read())
@@ -10,5 +11,20 @@ class neo4j_crud:
         url = "bolt://localhost:7687"
         self.driver= GraphDatabase.driver(url,auth=basic_auth(username,password))
 
+    def close(self):
+        self.driver.close()
+
+    def add_nodes(self):
+        with self.driver.session() as session:
+            pass
+            # Todo
+   
+    def read_nodes(self,tx):
+        for records in tx.run("MATCH (n) RETURN n "):
+            print(records)
+
+
 if __name__=="__main__":
     db= neo4j_crud()
+    with db.driver.session() as session:
+        session.read_transactions(db.read_nodes)
