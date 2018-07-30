ddcfc8fa4b12cc46b3c3695877a6abf96c1bee4a
concept-inversion <santosh.pandey2222@gmail.com>
2018-07-30 11:02:55 +0545

    adds a apoc procedure

--- neo4j-crud-py2neo.py
+++ neo4j-crud-py2neo.py
@@ -6,6 +6,10 @@ graph.create(hunter)
 query = '''
         MATCH(n) RETURN n
         '''
+query2 = '''
+        CALL apoc.help("dijkstra")
+        '''
 data = graph.run(query)
+strin = graph.run(query2)
 for d in data:
     print(d)
